ceb39bfc8533691a5aebc73e246431334036a303
Colten Kiehne <cokiehne@gmail.com>
2023-08-23 21:43:12 -0500

    Possible fix for touchscreen devices

--- Game-Planner/script.js
+++ Game-Planner/script.js
@@ -182,8 +182,15 @@ function handleClick(e) {
 
 /**Get mouse position on screen */
 function getPos(e) {
-  x = e.pageX - canvas.offsetLeft;
-  y = e.pageY - canvas.offsetTop;
+  if(e.type == 'touchstart' || e.type == 'touchmove' || e.type == 'touchend' || e.type == 'touchcancel'){
+    var evt = (typeof e.originalEvent === 'undefined') ? e : e.originalEvent;
+    var touch = evt.touches[0] || evt.changedTouches[0];
+    x = touch.pageX;
+    y = touch.pageY;
+} else if (e.type == 'mousedown' || e.type == 'mouseup' || e.type == 'mousemove' || e.type == 'mouseover'|| e.type=='mouseout' || e.type=='mouseenter' || e.type=='mouseleave') {
+    x = e.clientX;
+    y = e.clientY;
+}
   return { x, y };
 }
 
